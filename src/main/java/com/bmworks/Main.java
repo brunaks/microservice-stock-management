46b2d9e1ae183572142156d3ddb56cb154b69120
bruna <brunak_s@hotmail.com>
2020-02-05 21:50:57 -0300

    Add check stock endpoint

--- src/main/java/com/bmworks/Main.java
+++ src/main/java/com/bmworks/Main.java
@@ -6,18 +6,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+
 @SpringBootApplication
 public class Main {
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
     @EventListener(ApplicationReadyEvent.class)
-    public void onApplicationStart(ApplicationReadyEvent applicationReadyEvent) {
-
+    public void onApplicationStart(ApplicationReadyEvent applicationReadyEvent) throws UnknownHostException {
+        String port = applicationReadyEvent.getApplicationContext().getEnvironment().getProperty("server.port");
         RabbitTemplate rabbitTemplate = applicationReadyEvent.getApplicationContext().getBean(RabbitTemplate.class);
-        rabbitTemplate.convertAndSend("initialization", "stock-management-up", "Stock Management is up");
-
+        rabbitTemplate.convertAndSend("initialization", "stock-management-up", InetAddress.getLocalHost().getCanonicalHostName() + ":" + port);
     }
 }
---
+++ src/main/java/com/bmworks/rabbit/StockEndpoints.java
@@ -0,0 +1,15 @@
+package com.bmworks.rabbit;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.annotation.SessionScope;
+
+@RestController
+public class StockEndpoints {
+
+    @RequestMapping(path = "/getStock", method = RequestMethod.GET)
+    public int getStock(@RequestParam String productId) {
+        System.out.println("stock check called");
+        return 10;
+    }
+}
